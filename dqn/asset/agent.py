6cc2eb24baba2fcea924f1ac2892280aebd40206
kanokkorn <kanokorn@outlook.jp>
2019-08-15 05:39:13 +0700

    Create new folder

---
+++ dqn/asset/agent.py
@@ -0,0 +1,21 @@
+from __future__ import division
+
+import tensorflow as tf
+import numpy as np
+
+# don't know what todo next :/
+class agent(obj):
+    
+    def __init__(self):
+	
+	pass
+    
+    def network(self):
+	
+	pass
+
+# main function !
+if __name__ ==  "__main__":
+    
+    main()
+
