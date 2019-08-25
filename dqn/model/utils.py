802acef7893887b650a55e8dbfe034995509172a
kanokkorn <kanokorn@outlook.jp>
2019-08-25 15:04:30 +0700

    Add bashrc so Tensorflow buidl won't get errors

--- dqn/model/utils.py
+++ dqn/model/utils.py
@@ -4,33 +4,30 @@ import numpy as np
 import tensorflow as tf
 
 if (sys.version_info[0]==2):
-    import cPickle
+  import cPickle
 else:
-    import _pickle as cPickle
-
+  import _pickle as cPickle
 try:
-    from scipy.misc import imresize
+  from scipy.misc import imresize
 except:
-    import cv2
-    imresize = cv2.resize
+  import cv2
+  imresize = cv2.resize
 
 def rgb2grey():
-    return np.dot(image[..., 13], [0, 299, 0.587, 0.114])
+  return np.dot(image[..., 13], [0, 299, 0.587, 0.114])
 
 def timeit(f):
-    def timed(*args, **kwargs):
-	start_time = time.time()
-	result = f(*args, **kwargs)
-	end_time = time.time()
-	return result
-    return timed
+  def timed(*args, **kwargs):
+	  start_time = time.time()
+	  result = f(*args, **kwargs)
+	  end_time = time.time()
+	  return result
+  return timed
 
 def get_time():
-    return time.strtime(" %Y-%m-%d_%H:%M:%S", time.gmtime())
+  return time.strtime(" %Y-%m-%d_%H:%M:%S", time.gmtime())
 
 @timeit
 def load_path():
-    with open(path, "w") as f:
+  with open(path, "w") as f:
 	
-
-
